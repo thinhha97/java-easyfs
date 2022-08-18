eb6aae722e4f80dd0311239ae6b443df51480f2d
Thinh Ha <havanthinh97@gmail.com>
2022-08-18 13:12:55 +0700

    Initialize Spring boot

---
+++ src/main/java/dev/thinhha/easyfs/EasyfsApplication.java
@@ -0,0 +1,13 @@
+package dev.thinhha.easyfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyfsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/thinhha/easyfs/EasyfsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thinhha.easyfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
